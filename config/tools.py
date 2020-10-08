99fd9dbc2ff138c1b27a3694f34b1edc394d471e
Brofessional <rgoff@lanl.gov>
2020-10-08 17:07:03 -0600

    parser work

--- config/tools.py
+++ config/tools.py
@@ -18,6 +18,20 @@ def new_namespace(namespace_name, quota_files, quota_data, iperms, bperms):
 def get_repo(element_tree):
     pass
 
+def testing():
+    with open("test_file.xml", "r") as fp:
+        data = etree.parse(fp)
+
+    data = data.getroot()
+
+    for item in data:
+        if item.tag == "repo":
+            if item.attrib["name"] == "testone":
+                item.attrib["name"] = "testchange"
+
+    with open("test_file_out.xml", "w") as fp:
+        fp.write(etree.tostring(data).decode("utf-8"))
+
 
 if __name__ == '__main__':
-    pass
+    testing()
