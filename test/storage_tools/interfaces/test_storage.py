8bc219078eeeb1396fddf5413348cc2a98c45e02
Brofessional <rgoff@lanl.gov>
2020-10-13 13:37:59 -0600

    more parser stuff

--- test/storage_tools/interfaces/test_storage.py
+++ test/storage_tools/interfaces/test_storage.py
@@ -103,6 +103,7 @@ class TestZFSInterface:
             if "testing_datastore" in ds[0]:
                 found = True
         assert found
+
     @pytest.mark.skip
     def test_mount_datastore(self, marfs_config, working_repo):
         i = self.get_storage_interface(marfs_config, working_repo)
